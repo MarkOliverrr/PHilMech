46eddf03daf605245166ad6faea97fb56de75ebd
MarkOliverrr <olivergurtiza@gmail.com>
2026-01-26 13:45:00 +0800

    2

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PHilMech_Wprocure.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
